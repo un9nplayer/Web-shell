c87fe76325b58bec7190887af6e4306b009fcf66
Un9nPlayer <75741506+un9nplayer@users.noreply.github.com>
2024-04-22 21:09:36 +0530

    Add files via upload

---
+++ Anydesk.exe.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdCyDJGyX8VAjFC5aFQqyAE+1EbsQ5+n//NaGsl0RXeE+bIbU1KSKJt9CuhayJMJj33lV+A==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJhZkkaH1fi
+::ZQ05rAF9IBncCkqN+0xwdVsFHkrRbyXoZg==
+::ZQ05rAF9IAHYFVzEqQIHIRUUYAGFOWW+FdU=
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDDOLMiuWA7IZ5uzojw==
+::cRolqwZ3JBvQF1fEqQIHIRUUYAGFOWW+FfUa4eT+r+GOrEoONA==
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJG6L5kYjFB9VWAW+PXmuEboO46b+9+/n
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+Powershell -Command "Write-Host 'Hello, I'm @Un9nplayer!!!' -ForegroundColor Red"
+
+pause
